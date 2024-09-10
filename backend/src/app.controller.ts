3d711edcc1cedf1487ef9c2c7c33d6965ca25611
atmosphere1337 <agbdlcider@gmail.com>
2024-09-11 02:42:58 +0800

    passport.js jwt generate token

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -18,6 +22,6 @@ export class AppController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
