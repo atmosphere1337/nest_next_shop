368ab0906a911ae725c9b32f7908ce87199f554f
atmosphere1337 <agbdlcider@gmail.com>
2024-09-17 03:57:34 +0800

    Jest tests (2/2) Fix. Inject module dependencies in test files

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule, UsersModule, CacheModule.register({ ttl: 0 })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -16,7 +20,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello nest!!!');
     });
   });
 });
